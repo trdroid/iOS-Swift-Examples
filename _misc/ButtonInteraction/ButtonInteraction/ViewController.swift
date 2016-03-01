ca8312b80ac747d4757c5d4d4945143b29a3ecc8
gruprog <gruprog@gmail.com>
2016-03-01 13:55:04 -0500

    changes

--- _misc/ButtonInteraction/ButtonInteraction/ViewController.swift
+++ _misc/ButtonInteraction/ButtonInteraction/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var messageLabel: UILabel!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,8 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func onButtonPress(sender: UIButton) {
+	}
 
 }
 
