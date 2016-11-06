688c6319abc3f9a9db1e98f1e43c43b07b38263b
Jai Srikar <gruprog@gmail.com>
2016-11-06 10:05:14 -0500

    example on handling orientations

---
+++ Orientations/HandlingOrientations/HandlingOrientations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandlingOrientations
+//
+//  Created by droid on 2016-11-05.
+//  Copyright © 2016 droid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
